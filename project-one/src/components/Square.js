faef3d0fbcd32d471aec21337e96a1051196c04f
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-25 15:55:02 +0100

    tweaks to square components with changeable background colour

--- project-one/src/components/Square.js
+++ project-one/src/components/Square.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-const Square = () => <div className="red-square" style={{backgroundColor: 'red', width: '100px', height: '100px'}}></div>;
+const Square = ({ colour }) => (
+    <div className="square" 
+        style={{
+            backgroundColor: colour, 
+            width: '100px', 
+            height: '100px'}}>
+    </div>
+);
 
-export default Square;
+export default Square;  
