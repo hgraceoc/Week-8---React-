7598837f10ca31a4ad0b1d02771272af057d133c
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-26 10:52:12 +0100

    basket table component with sub basket item component

--- project-one/src/components/Stuff.js
+++ project-one/src/components/Stuff.js
@@ -3,6 +3,8 @@ import Header from "./Header";
 import Paragraph from "./Paragraph";
 import Square from "./Square";
 import People from "./People";
+import Basket from "./Basket";
+import BasketItem from "./BasketItem";
 
 const Stuff = () => (
     <div>
@@ -10,7 +12,19 @@ const Stuff = () => (
         <Paragraph>Hello hello hello</Paragraph>
         <Square />
         <People />
+        <Basket />
+        <BasketItem />
   </div>
 );
+
+Basket.defaultProps = {
+ items: [
+    { name: "Coffee", price: 2.10 },
+    { name: "Bananas", price: 3.50 },
+    { name: "Milk", price: 250.65 },
+    { name: "The Great Milk Shortage by Simon Schama", price: 12.99 },
+]
+
+}
 
 export default Stuff;
