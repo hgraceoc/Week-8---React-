9fa77747168a63d06c2b2f63085f453f90bf5428
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-26 12:17:24 +0100

    updated Paragraph.js to be a class based component

--- project-one/src/components/Paragraph.js
+++ project-one/src/components/Paragraph.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Paragraph = ({ children }) => (
-<p>{children ? <p>{ children }</p> : "Hello, world"} </p>
-);
+class Paragraph extends Component {
+    render() {
+    const { children } = this.props;
+    return (
+        <p>{children ? <p>{ children }</p> : "Hello, world"} </p>
+        );
+      }
+    }
 
 Paragraph.defaultProps = {
     children: "What a nice day for an ice cream and ice skating"
