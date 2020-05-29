6a1cb9e742a547cd62c1b7c54dd665e36f22b81e
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-29 10:03:23 +0100

    react notes to include pre made components and brief deployment notes

--- react notes.js
+++ react notes.js
@@ -455,3 +455,39 @@ export default Form;
 //PRE BUILT COMPONENTS
 
 //react bootstrap
+//add css style link to index.html
+//import component 
+
+//Button.js
+import React, { Component } from "react";
+
+import Button from 'react-bootstrap/Button'
+
+class Progress extends Component {
+    constructor(props){
+        super(props);
+    };
+
+render() {
+
+return (
+    <Button
+        variant="info">Hi
+    </Button>
+);
+}
+
+}
+
+export default Button; 
+
+
+//DEPLOYMENT
+
+//npm install gh-pages
+
+//package.json
+//"homepage": "https://hgraceoc.github.io/<repo>"
+
+//scripts
+//"deploy": "hg-pages -d build"
