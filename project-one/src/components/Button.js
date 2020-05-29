51df7b98ca23cd3b623f21e970aa065ee1c6dccf
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-29 10:04:42 +0100

    pre made component - react bootstrap button

---
+++ project-one/src/components/Button.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+import Button from 'react-bootstrap/Button'
+
+class Progress extends Component {
+    constructor(props){
+        super(props);
+    };
+
+render() {
+
+return (
+    <Button
+        variant="info">Hi
+    </Button>
+);
+}
+
+}
+
+
+
+export default Button; 
